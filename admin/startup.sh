869e783d4aaab50c8b1749c56b26b6cab0e27f05
韩孝冰 <bing@artboost.cn>
2023-08-21 20:15:52 +0800

    update

---
+++ admin/startup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cmd=$1
+cnt=$2
+# shellcheck disable=SC2164
+cd /home/ubuntu/stable-diffusion
+gpu_num=$(nvidia-smi -L | wc -l)
+for ((i = 0; i < cnt; i++)); do
+  port=$((7860 + i))
+  if [ "$cmd" = "start" ]; then
+    ./mstart.sh "$port" "$((i % gpu_num))"
+  elif [ "$cmd" = "stop" ]; then
+    ./mstop.sh "$port" "$((i % gpu_num))"
+  elif [ "$cmd" = "restart" ]; then
+    ./mstop.sh "$port" "$((i % gpu_num))"
+    ./mstart.sh "$port" "$((i % gpu_num))"
+  else
+    echo "unknown command $cmd"
+    exit 1
+  fi
+done
