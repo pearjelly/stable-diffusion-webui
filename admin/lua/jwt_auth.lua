ef666482f6f5969969d7f2d8b49a24edc6f648df
韩孝冰 <bing@artboost.cn>
2023-08-24 17:11:41 +0800

    update

--- admin/lua/jwt_auth.lua
+++ admin/lua/jwt_auth.lua
@@ -1,23 +1,15 @@
 local jwt = require "resty.jwt"
 local b64secret = "QE9CMGw9NSNMT0pkTFRkUmcmaGZqWWdUcUclSypLN1Y1"
 local token = ngx.var.cookie_webui_token
-print(token)
 if token == nil then
     ngx.exit(ngx.HTTP_UNAUTHORIZED)
 end
-local secret = ngx.decode_base64(b64secret)
-print(secret)
-local jwt_obj = jwt:verify(secret, token)
-local verified = jwt_obj["verified"]
-print(verified)
-if not verified then
+local jwt_obj = jwt:verify(ngx.decode_base64(b64secret), token)
+if not jwt_obj["verified"] then
     ngx.exit(ngx.HTTP_UNAUTHORIZED)
 end
 local exp = jwt_obj["payload"]["exp"]
-print(exp)
 if exp < ngx.time() then
     ngx.exit(ngx.HTTP_UNAUTHORIZED)
 end
-local instance = jwt_obj["payload"]["instance"]
-print(instance)
-ngx.var.instance = instance
+ngx.var.instance = jwt_obj["payload"]["instance"]
